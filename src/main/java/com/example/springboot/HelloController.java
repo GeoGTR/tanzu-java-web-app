120f14994930e62645c70f52a973dddf18052a40
Fatih Özgür <fatihozgurcmpe@gmail.com>
2023-07-08 22:28:02 +0300

    Test commit v3

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! - Commit v2";
+		return "Greetings from Spring Boot + Tanzu! - Test Commit v3";
 	}
 
 }
